059dcef83070b7f3b2b8d5a0c8514e4fc29d40b2
Marius Sørensen <jobb@s0rensen.no>
2022-05-10 16:46:19 +0200

    Enable production

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,4 +1,4 @@
-bool is_production = false;
+bool is_production = true;
 String api_url = is_production
     ? "https://ar-for-education-api.azurewebsites.net/"
     : "https://6910-84-213-22-174.eu.ngrok.io/";
